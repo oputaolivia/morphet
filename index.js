1e57f7ea590f9f8d1d866c03c98f411cc8ef6dde
oputaolivia <oputaolivia123@gmail.com>
2023-07-14 00:30:28 +0100

    Delete File

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 import { program } from 'commander';
 import {crtFile}  from '../morph/commands/createFile.js';
 import {readfile} from './commands/readFile.js';
+import { wrtFile } from './commands/writeFile.js';
+import {delFile} from './commands/deleteFile.js';
 
 const command = 'morph'
-program.command(`write <fileName> [content]`)
-    .description('Create a file')
+program.command(`create <fileName>`)
+    .description('Create an empty file')
     .action(crtFile);
+
+program.command(`write <fileName> [content]`)
+    .description('Write into a file')
+    .action(wrtFile);
 
 program.command(`read <filename>`)
     .description(`Read file Content`)
     .action(readfile)
+
+program.command(`del <fileName..>`)
+.description('Delete File')
+.option('-d, --del, <del..>')
+.action(delFile)
 program.parse(process.argv);
