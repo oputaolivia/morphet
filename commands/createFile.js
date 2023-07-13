397e14f1324c508a8e641822a327b04a38f6ed5a
oputaolivia <oputaolivia123@gmail.com>
2023-07-13 22:15:39 +0100

    Edited Create File

--- commands/createFile.js
+++ commands/createFile.js
@@ -1,11 +1,11 @@
 import fs from 'fs-extra';
 import chalk from 'chalk';
 
-export const crtFile = async (fileName, content) =>{
+export const crtFile = async (fileName) =>{
    try{
     const fileExist = fs.existsSync(fileName);
     if (!fileExist){
-        await fs.writeFile(fileName,content, 'utf8');
+        await fs.createFile(fileName)
         console.log(chalk.greenBright.bold(`${fileName} created`))
     }else{
         console.log(chalk.redBright.bold('File Exists'))
